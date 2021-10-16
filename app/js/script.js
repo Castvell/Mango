dd278643b2e4d10e9f2b2747a5707edc6fbc194a
Castvell <miguelcastellanos2000@hotmail.com>
2021-10-15 22:02:57 -0400

    Added customer section w not images yet, header mobile bug fixed

--- app/js/script.js
+++ app/js/script.js
@@ -4,6 +4,10 @@ const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
 
+const option1 = document.querySelector('#mobile_header1');
+const option2 = document.querySelector('#mobile_header2');
+const option3 = document.querySelector('#mobile_header3');
+
 btnHamburger.addEventListener('click', function(){
   console.log('click hamburger');
 
@@ -25,4 +29,52 @@ btnHamburger.addEventListener('click', function(){
     });
 
   }  
+});
+
+option1.addEventListener('click', function(){
+  console.log('click option1')
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+   
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+
+    });
+    
+  }
+});
+
+option2.addEventListener('click', function(){
+  console.log('click option1')
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+   
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+      
+    });
+    
+  }
+});
+
+option3.addEventListener('click', function(){
+  console.log('click option1')
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+   
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+      
+    });
+    
+  }
 });
