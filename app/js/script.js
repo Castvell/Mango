dd3038ab4771d1e04a8e0944e17a772a185bc987
Castvell <miguelcastellanos2000hotmail.com>
2021-10-23 14:38:12 -0400

    Demo PopUp additioned, for now it isnt saving form info

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,48 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const btnDemo1 =document.querySelector('#btn1');
+const btnDemo2 =document.querySelector('#btn2');
+const btnDemo3 =document.querySelector('#btn3');
+const btnPopup =document.querySelector('#btnPopup');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
 
+const inputs = document.querySelectorAll(".input");
+
 const option1 = document.querySelector('#mobile_header1');
 const option2 = document.querySelector('#mobile_header2');
 const option3 = document.querySelector('#mobile_header3');
+
+//popup
+btnDemo1.addEventListener('click', function(){
+  console.log('click demo1');
+  document.querySelector('.popup').classList.add("active");
+  document.querySelector('.dm').classList.add("activo");
+  body.classList.add('noscroll');
+});
+
+btnDemo2.addEventListener('click', function(){
+  console.log('click demo1');
+  document.querySelector('.popup').classList.add("active");
+  document.querySelector('.dm').classList.add("activo");
+  body.classList.add('noscroll');
+});
+
+btnDemo3.addEventListener('click', function(){
+  console.log('click demo1');
+  document.querySelector('.popup').classList.add("active");
+  document.querySelector('.dm').classList.add("activo");
+  body.classList.add('noscroll');
+});
+
+btnPopup.addEventListener('click', function(){
+  console.log('click enviar');
+  document.querySelector('.popup').classList.remove("active");
+  document.querySelector('.dm').classList.remove("activo");
+  body.classList.remove('noscroll');
+});
+//
 
 btnHamburger.addEventListener('click', function(){
   console.log('click hamburger');
@@ -77,4 +113,21 @@ option3.addEventListener('click', function(){
     });
     
   }
+});
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
 });
